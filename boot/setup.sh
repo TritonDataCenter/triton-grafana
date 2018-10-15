d615b9adcd177756479af4e4d19a46ea60cd01db
Isaac Davis <isaac.davis@joyent.com>
2018-10-15 14:24:24 -0700

    more grafana setup

--- boot/setup.sh
+++ boot/setup.sh
@@ -35,7 +35,9 @@ DATA_DIR=$GRAF_PERSIST_DIR/data
 CONF_DIR=$GRAF_PERSIST_DIR/conf
 TLS_DIR=/data/tls
 
-SAPI_INST_DATA_JSON=$GRAF_PERSIST_DIR/config.json
+CONFIG_FILE=${CONF_DIR}/grafana.ini
+DATASOURCES_FILE=${CONF_DIR}/provisioning/datasources/triton.yaml
+DASHBOARDS_FILE=${CONF_DIR}/provisioning/dashboards/triton.yaml
 
 # - TLS cert
 CERT_FILE=$TLS_DIR/cert.pem
@@ -49,7 +51,6 @@ function fatal {
     exit 1
 }
 
-
 # Mount our delegated dataset at /data.
 function grafana_setup_delegate_dataset() {
     local dataset
@@ -62,17 +63,13 @@ function grafana_setup_delegate_dataset() {
     fi
 }
 
-
 # Setup key and client certificate used to auth with this DC's CMON.
 function grafana_setup_certs() {
-    local cert_name
-
     if [[ -f "$CERT_FILE" && -f "$CERT_KEY_FILE" ]]; then
         echo "Key files already exist: $CERT_FILE, $CERT_KEY_FILE"
     else
         echo "Generating tls cert and key for CMON auth"
         mkdir -p $TLS_DIR
-        key_name=grafana-$(zonename | cut -d- -f1)-$(date -u '+%Y%m%dT%H%M%S')
         # Create cert and key
         openssl req -x509 -nodes -subj "/CN=admin" -newkey rsa:2048 \
             -keyout $CERT_KEY_FILE -out $CERT_FILE -days 365
@@ -81,7 +78,6 @@ function grafana_setup_certs() {
     return 0
 }
 
-
 function grafana_setup_env {
     if ! grep grafana /root/.profile >/dev/null; then
         echo "" >>/root/.profile
@@ -89,13 +85,71 @@ function grafana_setup_env {
     fi
 }
 
+function grafana_ensure_nobody_owner() {
+    local output
+
+    output=$(chown -c nobody:nobody \
+        $CONFIG_FILE \
+        $DATASOURCES_FILE \
+        $DASHBOARDS_FILE \
+        $CERT_FILE \
+        $CERT_KEY_FILE \
+        /data/prometheus/data)
+    if [[ -n "$output" ]]; then
+        echo "$output"
+    fi
+
+    return 0
+}
+
+# Enable/restart/clear grafana, if necessary.
+function grafana_restart_grafana() {
+    local currState
+
+    currState=$(svcs -Ho state grafana)
+    if [[ "$currState" == "disabled" ]]; then
+        # Zone setup starts with grafana in disabled state. We enable it
+        # after the config is generated for the first time.
+        echo "Enabling grafana SMF service"
+        svcadm enable grafana
+    elif [[ "$currState" == "online" ]]; then
+            svcadm restart grafana
+        fi
+    elif [[ "$currState" == "maintenance" ]]; then
+        echo "Clearing grafana SMF service"
+        svcadm clear grafana
+    else
+        fatal "unexpected grafana service state: '$currState'"
+    fi
+
+    return 0
+}
+
+function grafana_write_config {
+    local config_file=$1
+    local basename=$(basename ${config_file})
+
+    if [[ ! -f ${config_file}.new ]]; then
+        fatal "${config_file}.new not found"
+    fi
+
+    # Update the config, if changed.
+    if [[ ! -f ${config_file} ]]; then
+        # First time config.
+        echo "Writing first time grafana config ($config_file)"
+        mv ${config_file}.new ${config_file}
+    elif ! diff ${config_file} ${config_file}.new >/dev/null; then
+        # The config differs.
+        echo "Updating prometheus config ${basename}"
+        cp ${config_file} ${config_file}.bak
+        mv ${config_file}.new ${config_file}
+    fi
+}
 
 function grafana_setup_grafana {
-    local config_file
     local dc_name
     local dns_domain
 
-    config_file=$CONF_DIR/grafana.ini
     dc_name=$(mdata-get sdc:datacenter_name)
     dns_domain=$(mdata-get sdc:dns_domain)
     if [[ -z "$dns_domain" ]]; then
@@ -105,26 +159,73 @@ function grafana_setup_grafana {
     fi
 
     mkdir -p $DATA_DIR
-    mkdir -p $CONF_DIR
+    mkdir -p $CONF_DIR/provisioning/dashboards
+    mkdir -p $CONF_DIR/provisioning/datasources
+    
+    
 
-    # This is disabled by default. It is up to 'prometheus-configure' to
-    # enable it.
+    cat > ${CONFIG_FILE}.new <<CONFIGINI
+# config file version
+apiVersion: 1
+
+[server]
+http_port=443
+protocol=https
+cert_file=${CERT_FILE}
+cert_key=${CERT_KEY_FILE}
+
+[paths]
+data=${DATA_DIR}
+provisioning=${CONF_DIR}/provisioning
+CONFIGINI
+
+    cat > ${DATASOURCES_FILE}.new <<DATAYML
+# config file version
+apiVersion: 1
+
+datasources:
+    - name: Triton
+      type: prometheus
+      access: proxy
+      orgId: 1
+      url: http://prometheus.${dc_name}.${dns_domain}:9090
+      isDefault: true
+      editable: true
+DATAYML
+
+cat > ${DASHBOARDS_FILE}.new <<DASHYML
+# config file version
+apiVersion: 1
+
+providers:
+    - name: Triton
+      orgId: 1
+      folder: ''
+      type: file
+      options:
+        path: /opt/triton/grafana/dashboards
+DASHYML
+
+    grafana_write_config ${CONFIG_FILE}
+    grafana_write_config ${DATASOURCES_FILE}
+    grafana_write_config ${DASHBOARDS_FILE}
+
     /usr/sbin/svccfg import /opt/triton/grafana/smf/manifests/grafana.xml
 
     return 0
 }
-
 
 # ---- mainline
 
 grafana_setup_delegate_dataset
 grafana_setup_certs
 grafana_setup_env
+grafana_ensure_nobody_owner
 
 # Before 'sdc_common_setup' so the grafana SMF service is imported before
 # config-agent is first setup.
 grafana_setup_grafana
-
+grafana_restart_grafana
 
 CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/triton/grafana
 source /opt/smartdc/boot/lib/util.sh
